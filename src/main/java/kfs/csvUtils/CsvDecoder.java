2ca3b5d2a903b79042cb2b470bfbcb1f818007bc
kofis <k0fis@me.com>
2015-01-15 06:45:55 +0100

    Add KfsField

--- src/main/java/kfs/csvUtils/CsvDecoder.java
+++ src/main/java/kfs/csvUtils/CsvDecoder.java
@@ -1,8 +1,6 @@
 package kfs.csvUtils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -10,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.persistence.Column;
+import kfs.field.KfsField;
 import kfs.utils.KfsSpringUtilsException;
 
 /**
@@ -18,92 +18,55 @@ import kfs.utils.KfsSpringUtilsException;
  */
 public class CsvDecoder {
 
-    private final Map<Class, Map<Integer, Method>> csvMaps;
-    private final Map<Method, DateFormat> tsDecoder;
+    private final Map<Integer, KfsField> fieldMap;
+    private final Map<KfsField, DateFormat> tsDecoder;
+    private final Map<KfsField, Integer> fieldLength;
 
-    public CsvDecoder(Class... preps) {
-        csvMaps = new HashMap<Class, Map<Integer, Method>>();
-        tsDecoder = new HashMap<Method, DateFormat>();
-        for (Class c : preps) {
-            getMap(c);
-        }
-
-    }
-
-    private synchronized Map<Integer, Method> getMap(Class cls) {
-        if (csvMaps.containsKey(cls)) {
-            return csvMaps.get(cls);
-        }
-        Map<Integer, Method> map = new HashMap<Integer, Method>();
+    public CsvDecoder(Class cls) {
+        fieldMap = new HashMap<Integer, KfsField>();
+        tsDecoder = new HashMap<KfsField, DateFormat>();
+        fieldLength = new HashMap<KfsField, Integer>();
         Field fields[] = cls.getDeclaredFields();
         for (Field field : fields) {
             CsvPos pos = field.getAnnotation(CsvPos.class);
             if (pos != null) {
-                Method m = findSetter(cls, field.getName(), field.getType());
-                map.put(pos.value(), m);
+                KfsField kf = new KfsField(cls, field);
+                fieldMap.put(pos.value(), kf);
                 CsvTsFormat fmt = field.getAnnotation(CsvTsFormat.class);
                 if (fmt != null) {
-                    tsDecoder.put(m, new SimpleDateFormat(fmt.value()));
+                    tsDecoder.put(kf, new SimpleDateFormat(fmt.value()));
+                }
+                Column column = field.getAnnotation(Column.class);
+                if (column != null) {
+                    fieldLength.put(kf, column.length());
                 }
             }
-        }
-        csvMaps.put(cls, map);
-        return map;
-    }
-
-    private void setVal(Method setter, Object ret, Object value) {
-        try {
-            setter.invoke(ret, value);
-        } catch (IllegalAccessException ex) {
-            throw new KfsSpringUtilsException("Cannot set " 
-                    + ret.getClass().getSimpleName() + "."
-                    + setter.getName(), ex);
-        } catch (IllegalArgumentException ex) {
-            throw new KfsSpringUtilsException("Cannot set " 
-                    + ret.getClass().getSimpleName() + "."
-                    + setter.getName(), ex);
-        } catch (InvocationTargetException ex) {
-            throw new KfsSpringUtilsException("Cannot set " 
-                    + ret.getClass().getSimpleName() + "."
-                    + setter.getName(), ex);
-        }
-    }
-
-    private Method findGetter(Class<?> theClass, String property, Class paramType) {
-        return findSGetter("get", theClass, property);
-    }
-
-    private Method findSetter(Class<?> theClass, String property, Class paramType) {
-        return findSGetter("set", theClass, property, paramType);
-    }
-
-    private Method findSGetter(String prefix, Class<?> theClass, String property, Class... paramType) {
-        String setter = prefix + String.format("%C%s", property.charAt(0), property.substring(1));
-        try {
-            return theClass.getMethod(setter, paramType);
-        } catch (NoSuchMethodException ex) {
-            throw new KfsSpringUtilsException("Cannot find method " + setter 
-                    + " for field " + property, ex);
         }
     }
 
     public <T> T readEdw(String[] line, Class<T> cls, T ret) {
-        Map<Integer, Method> setterMap = getMap(cls);
         for (int inx = 0; inx < line.length; inx++) {
             if (line[inx].isEmpty()) {
                 continue;
             }
-            Method m = setterMap.get(inx);
+            KfsField m = fieldMap.get(inx);
             if (m != null) {
-                Class pcls = m.getParameterTypes()[0];
+                Class pcls = m.getFieldType();
                 if (String.class.equals(pcls)) {
-                    setVal(m, ret, line[inx]);
+                    Integer cl = fieldLength.get(m);
+                    if (cl != null) {
+                        if (line[inx].length() > cl) {
+                            throw new KfsSpringUtilsException("Column oversize, cannot set "
+                                    +m.getName()+", inx: "+inx+" with value: " + line[inx]);
+                        }
+                    }
+                    m.setVal(ret, line[inx]);
                 } else if (Double.class.equals(pcls)) {
-                    setVal(m, ret, Double.parseDouble(line[inx]));
+                    m.setVal(ret, Double.parseDouble(line[inx]));
                 } else if (Integer.class.equals(pcls)) {
-                    setVal(m, ret, Integer.parseInt(line[inx]));
+                    m.setVal(ret, Integer.parseInt(line[inx]));
                 } else if (Long.class.equals(pcls)) {
-                    setVal(m, ret, Long.parseLong(line[inx]));
+                    m.setVal(ret, Long.parseLong(line[inx]));
                 } else if (Timestamp.class.equals(pcls)) {
                     DateFormat f = tsDecoder.get(m);
                     if (f == null) {
@@ -118,7 +81,7 @@ public class CsvDecoder {
                                 + "\" - value on " + inx + " position: \"" + line[inx] + "\"");
                     }
                     Timestamp ts = new Timestamp(d.getTime());
-                    setVal(m, ret, ts);
+                    m.setVal(ret, ts);
                 }
             }
 
@@ -128,27 +91,13 @@ public class CsvDecoder {
 
     public CharSequence toString(String[] line, Object obj) {
 
-        Map<Integer, Method> map = new HashMap<Integer, Method>();
-        if (obj != null) {
-            Field fields[] = obj.getClass().getDeclaredFields();
-            for (Field field : fields) {
-                CsvPos pos = field.getAnnotation(CsvPos.class);
-                if (pos != null) {
-                    map.put(pos.value(), findGetter(obj.getClass(), field.getName(), field.getType()));
-                }
-            }
-        }
         StringBuilder sb = new StringBuilder();
         for (int inx = 0; inx < line.length; inx++) {
             sb.append(String.format("\n%30s", line[inx]));
-            Method m = map.get(inx);
+            KfsField m = fieldMap.get(inx);
             if (m != null) {
-                try {
-                    sb.append(" - ").append(String.format("%30s", m.invoke(obj)))//
-                            .append(" : ").append(m.getName().substring(3));
-                } catch (Exception ex) {
-                    throw new KfsSpringUtilsException("Cannot Call Method " + m.getName(), ex);
-                }
+                sb.append(" - ").append(String.format("%30s", m.getVal(obj)))//
+                        .append(" : ").append(m.getName().substring(3));
             }
         }
         sb.append("\n");
---
+++ src/main/java/kfs/field/KfsField.java
@@ -0,0 +1,89 @@
+package kfs.field;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import kfs.utils.KfsSpringUtilsException;
+
+/**
+ *
+ * @author pavedrim
+ */
+public class KfsField {
+
+    private final Class cls;
+    private final Field field;
+    private Method setFieldMethod;
+    private Method getFieldMethod;
+
+    public KfsField(Class cls, Field field) {
+        this.cls = cls;
+        this.field = field;
+    }
+    
+    private Method findSGetter(String prefix, Class<?> theClass, String property, Class... paramType) {
+        String setter = prefix + String.format("%C%s", property.charAt(0), property.substring(1));
+        try {
+            return theClass.getMethod(setter, paramType);
+        } catch (NoSuchMethodException ex) {
+            throw new KfsSpringUtilsException("Cannot find method " + setter
+                    + " for field " + property, ex);
+        }
+    }
+
+    public String getName() {
+        return field.getName();
+    }
+    
+    public Class getFieldType() {
+        return field.getType();
+    }
+    
+    public Field getField() {
+        return field;
+    }
+
+    protected Method getSetFieldMethod() {
+        if (setFieldMethod == null) {
+            setFieldMethod = findSGetter("set", cls, field.getName(), field.getType());
+        }
+        return setFieldMethod;
+    }
+
+    protected Method getGetFieldMethod() {
+        if (getFieldMethod == null) {
+            getFieldMethod = findSGetter("get", cls, field.getName());
+        }
+        return getFieldMethod;
+    }
+    
+    public void setVal(Object ret, Object value) {
+        try {
+            getSetFieldMethod().invoke(ret, value);
+        } catch (IllegalAccessException ex) {
+            throw new KfsSpringUtilsException("Cannot set " 
+                    + field.getName() +" with value: "
+                    + value, ex);
+        } catch (IllegalArgumentException ex) {
+            throw new KfsSpringUtilsException("Cannot set " 
+                    + field.getName() +" with value: "
+                    + value, ex);
+        } catch (InvocationTargetException ex) {
+            throw new KfsSpringUtilsException("Cannot set " 
+                    + field.getName() +" with value: "
+                    + value, ex);
+        }
+    }    
+
+    public Object getVal(Object ret) {
+        try {
+            return getGetFieldMethod().invoke(ret);
+        } catch (IllegalAccessException ex) {
+            throw new KfsSpringUtilsException("Cannot get " + field.getName(), ex);
+        } catch (IllegalArgumentException ex) {
+            throw new KfsSpringUtilsException("Cannot get " + field.getName(), ex);
+        } catch (InvocationTargetException ex) {
+            throw new KfsSpringUtilsException("Cannot get " + field.getName(), ex);
+        }
+    }
+}
